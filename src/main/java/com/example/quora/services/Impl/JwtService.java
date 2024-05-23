ca90b2d294c8b08a5a3a9168d6ccf790b772b88b
dhavalkgit <dhavalk439@gmail.com>
2024-05-23 15:07:23 +0530

    some changes is done
    ces/Impl/JwtService.java

--- src/main/java/com/example/quora/services/Impl/JwtService.java
+++ src/main/java/com/example/quora/services/Impl/JwtService.java
@@ -4,13 +4,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -61,7 +59,7 @@ public class JwtService {
         return new Date().before(exp);
     }
 
-    public Boolean isTokenValid(String token, String email){
-        return email.equals(extractClaim(token,"email")) && tokenExpired(token);
+    public Boolean isTokenValid(String token, String userName){
+        return userName.equals(extractClaim(token,"email")) && tokenExpired(token);
     }
 }
